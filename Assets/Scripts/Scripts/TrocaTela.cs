995c0d8eaab4aa6b185a1caa6bc686ed02db9d59
Laura Schiavon <lauraschiavon00@gmail.com>
2024-06-05 22:48:04 -0300

    Correção Troca Tela

--- Assets/Scripts/Scripts/TrocaTela.cs
+++ Assets/Scripts/Scripts/TrocaTela.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class TrocaTela : MonoBehaviour
 {
-    public void trocarParaCredito()
-    {
-        var start = SceneManager.GetActiveScene();
-        var sceneLoad = SceneManager.LoadSceneAsync("CenaCredito");
-        sceneLoad.allowSceneActivation = true;
-    }
-
     public void trocarParaInicial()
     {
-        var start = SceneManager.GetActiveScene();
-        var sceneLoad = SceneManager.LoadSceneAsync("CenaInicial");
-        sceneLoad.allowSceneActivation = true;
+         SceneManager.LoadScene("CenaInicial");  
     }
 
+    public void trocarParaCredito()
+    {
+        SceneManager.LoadScene("CenaCredito");
+    }
 }
