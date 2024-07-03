1f026dd2c6af07ff67f2c9d7b789bce5baef021e
Luis Filipe Tinos <lftinos@gmail.com>
2024-07-03 19:30:46 -0300

    Remoção de scripts duplicados

--- Assets/Memoria/Scripts/JogoMemoriaImagemCard.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Jogo da Memoria/Carta Imagem")]
-public class JogoMemoriaImagemCard : ScriptableObject
-{
-    public Sprite cardSprite;
-}
